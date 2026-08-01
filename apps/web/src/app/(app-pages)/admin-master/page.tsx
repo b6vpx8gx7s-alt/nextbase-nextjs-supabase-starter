b7f26763abfbea28393ed76777d10035754b5993
Andres <andres-orellano@hotmail.com>
2026-08-01 03:49:42 -0500

    fix: remove residual admin-master folder causing build error

--- apps/web/src/app/(app-pages)/admin-master/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getAdminBusinesses } from '@/data/admin/businesses';
-import { AdminMasterClient } from './AdminMasterClient';
-
-export const dynamic = 'force-dynamic';
-
-export default async function AdminMasterPage() {
-  const businesses = await getAdminBusinesses();
-  return <AdminMasterClient businesses={businesses} />;
-}
